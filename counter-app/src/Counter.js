42721fc7c614672858599ed6cbf581cc71e4ea83
Johnathan Mial <jfmial@student.fullsail.edu>
2024-10-11 16:31:47 -0700

    imported two react methods for Counter logic

--- counter-app/src/Counter.js
+++ counter-app/src/Counter.js
@@ -1,12 +1,27 @@
+// access methods defined in the “react” module
+import { useState, useEffect } from "react";
+
+
 // a Counter component that displays an incrementing counter automatically every second
 function Counter () {
-    var count = 0;
+    //array destructuring
+    const [count, setCount] = useState(0);
+
+    useEffect(function() {
+        var timer = setInterval(function() {
+            setCount(count+1);
+            console.log("count =", count);
+        }, 1000);
+
+        return function() {
+            clearInterval(timer);
+        }
+        
+    })
+    
     
     //increment the displayed counter, which currently remains at 0.
-    setInterval(function() {
-        count++;
-        console.log("count =", count);
-    }, 1000);
+    
 
     return(
         <>
